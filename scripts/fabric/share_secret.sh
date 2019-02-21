d666c2db6e99efd72270fe9d3316d51f385b5e2d
Rahul Mahadev <rahul.mahadev7@gmail.com>
2019-02-21 08:16:32 -0600

    Modified share secret script

--- scripts/fabric/share_secret.sh
+++ scripts/fabric/share_secret.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 CMD_PRE_STMT="CORE_PEER_ADDRESS=peer1.org1.example.com:7051"
-CMD_STMT="peer chaincode query -C mychannel -n honeybadgerscc -c '{\"Args\":[\"hbmpc\",\"a\"]}'"
+CMD_STMT="peer chaincode query -C mychannel -n honeybadgerscc -c '{\"Args\":[\"hbmpc\","
+END_STMT="]}'"
 LOCAL_CMD="python3.7 -m honeybadgermpc.secretshare_hbavsslight 4 conf/hbavss.hyper.ini"
 CONFIG_PATH=conf/hbavss.local.ini
 
 # Generate config file locally
 set -x
 tmux new-session     "${LOCAL_CMD}; sh" \; \
-    splitw -h -p 50 "CORE_PEER_ADDRESS=peer0.org1.example.com:7051 ${CMD_STMT}; sh" \; \
-     splitw -v -p 50 "CORE_PEER_ADDRESS=peer1.org1.example.com:7051 ${CMD_STMT}; sh" \; \
+    splitw -h -p 50 "CORE_PEER_ADDRESS=peer0.org1.example.com:7051 ${CMD_STMT}  "\"0\""  ${END_STMT}; sh" \; \
+     splitw -v -p 50 "CORE_PEER_ADDRESS=peer1.org1.example.com:7051 ${CMD_STMT}  "\"1\"" ${END_STMT}; sh" \; \
      selectp -t 0 \; \
-     splitw -v -p 50 "CORE_PEER_ADDRESS=peer0.org2.example.com:7051 ${CMD_STMT}; sh" \; \
-     splitw -v -p 50 "CORE_PEER_ADDRESS=peer1.org2.example.com:7051 ${CMD_STMT}; sh"
+     splitw -v -p 50 "CORE_PEER_ADDRESS=peer0.org2.example.com:7051 ${CMD_STMT} "\"2\"" ${END_STMT}  ; sh" \; \
+     splitw -v -p 50 "CORE_PEER_ADDRESS=peer1.org2.example.com:7051 ${CMD_STMT}  "\"3\"" ${END_STMT}; sh"
