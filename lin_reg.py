159758b2b9d3fd7801ab646e2e1b3677e68f68d2
rahul.mahadev7@gmail.com <rahul.mahadev7@gmail.com>
2019-04-07 07:05:46 +0000

    Updated linear regression

--- lin_reg.py
+++ lin_reg.py
@@ -62,7 +62,8 @@ def mpc(eqexample_name, chaincode):
 		print("Failed to get result")
 		
 	if res == 'None':
-		open_secrets(eqexample_name, chaincode)
+		print("failed to start MPC")
+		# open_secrets(eqexample_name, chaincode)
 	else:
 		print("################# Started MPC  #################")
 			
@@ -96,7 +97,7 @@ def create_eqexample(chaincode, num_vals):
 	eqexample_name = 'test'
 	time_limit = num_vals
 	user_name = 'user1'
-	cmd_list = ['peer','chaincode', 'invoke','-C' ,'mychannel' , '-c','{"Args":["createInstance", "' + eqexample_name + '","'  + '7' +  '","' + user_name + '"  ]}','-n' ,chaincode ,'--peerAddresses', 'peer0.org1.example.com:7051', '--peerAddresses', 'peer1.org1.example.com:7051', '--peerAddresses','peer0.org2.example.com:7051','--peerAddresses', 'peer1.org2.example.com:7051']
+	cmd_list = ['peer','chaincode', 'invoke','-C' ,'mychannel' , '-c','{"Args":["createInstance", "' + eqexample_name + '","'  + num_vals +  '","' + user_name + '"  ]}','-n' ,chaincode ,'--peerAddresses', 'peer0.org1.example.com:7051', '--peerAddresses', 'peer1.org1.example.com:7051', '--peerAddresses','peer0.org2.example.com:7051','--peerAddresses', 'peer1.org2.example.com:7051']
 	task = subprocess.run(cmd_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out = task.stdout
 	err = task.stderr	
@@ -135,16 +136,15 @@ def join_eqexample(chaincode, x, y):
 	print("Dealing Shares to endorsers.....")
 	print(local_cmd)
 	task = subprocess.run(local_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	time.sleep(2)
+	#print(task.stderr)
+	#print(task.stdout)
+	time.sleep(0)
 	local_cmd = ['python3.7', '-m', 'honeybadgermpc.fabric_share_secret' , str(y) ,cellname_y, 'rps']
 	print("Dealing Shares to endorsers.....")
 	print(local_cmd)
 	task = subprocess.run(local_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	# print(task.stderr)
-	# print(task.stdout)
 	print("#######################################")
 	print("Successfully shared secret")
-	print("Now wait for the timeout to view the result")
 	# print(inp)
 	# time.sleep(6)
 	# mpc(eqexample_name, chaincode)
@@ -152,45 +152,24 @@ def join_eqexample(chaincode, x, y):
 	# get_result(eqexample_name, chaincode)
 
 def main():
-	if(len(sys.argv) < 2):
+	if(len(sys.argv) < 3):
 		print("Enter the name of the chaincode as argument")
 		sys.exit(0)
 
 	chaincode_name = sys.argv[1]
 	print('Linear Regression tester')
-	print('CREATING MPC INSTANCE')
-	create_eqexample(chaincode_name, '8')
-	time.sleep(2)
-	print('AVSSing the inputs')
-	Xs = [1,2,3,4,5,6,7,8]
-	Ys = [2,3,4,5,6,7,8,9]
-	for i in range len(Xs):
-		join_eqexample(chaincode_name, Xs[i], Ys[i])
+	if sys.argv[2] != 'skip':
+		print('CREATING MPC INSTANCE')
+		create_eqexample(chaincode_name, '3')
 		time.sleep(2)
-
-	# get_result('test',chaincode_name)
-	# sys.exit(0)
-	# print('###################################')
-	# print('# MPC Example 		###')
+		print('AVSSing the inputs')
+		Xs = [1,2,3,4,5,6,7,8]
+		Ys = [2,3,4,5,6,7,8,9]
+		for i in range(3):
+			join_eqexample(chaincode_name, Xs[i], Ys[i])
+			time.sleep(2)
 	
-	# while(1):
-	#	print("#############################################")
-	#	print("1. Press 1 to View all active MPC sessions ")
-	#	print("2. Press 2 to View all completed MPC session ")
-	#	print("3. Press 3 to create an MPC session ")
-	#	print("4. Press 4 to enter input to an MPC session ")
-	#	print("5. Press 5 to exit ")
-	#	inp = input()
-	#	if inp == '1':
-	#		all_eqexamples(chaincode_name)
-	#	elif inp == '2':
-	#		completed_eqexamples(chaincode_name)
-	#	elif inp == '3':
-	#		create_eqexample(chaincode_name)
-	#	elif inp == '4':
-	#		join_eqexample(chaincode_name)
-	#	elif inp == '5':
-	#		sys.exit(0)	
-
+	mpc('test',chaincode_name)
+	
 if __name__ == '__main__':
 	main()
