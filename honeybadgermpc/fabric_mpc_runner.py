9e880a5cce178ffbe3939c10e180dfddd0698f93
Rahul Mahadev <rahul.mahadev7@gmail.com>
2019-04-15 01:24:08 -0500

    trying something

--- honeybadgermpc/fabric_mpc_runner.py
+++ honeybadgermpc/fabric_mpc_runner.py
@@ -339,7 +339,7 @@ async def linear_regression_wrapper(context, **kwargs):
 async def auction_mpc(context, pp_elements, bids):
     max_bid = bids[0]
     for i in range(1, len(bids)):
-        t = await max_bid.lt(bids[i])
+        t = await max_bid.gt(bids[i])
         t = FixedPoint(context, t, pp=pp_elements)
         max_bid = (await (t.mul(max_bid.add(bids[i])))).sub(max_bid)
     return max_bid
